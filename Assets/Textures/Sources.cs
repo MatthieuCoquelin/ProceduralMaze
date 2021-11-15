761ec4db1ceb1f13cd602daf998bc311ed0c1c79
MatthieuCoquelin <32342575+MatthieuCoquelin@users.noreply.github.com>
2021-11-15 13:22:49 +0100

    Delete Sources.cs

--- Assets/Textures/Sources.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Sources : MonoBehaviour
-{
-    //https://stock.adobe.com/220812447?as_campaign=TinEye&as_content=tineye_match&epi1=220812447&tduid=50f7ff1d1263a45c26f4de7ed20fca88&as_channel=affiliate&as_campclass=redirect&as_source=arvato
-}
